209b34db555e912908d1dbfe79db7b9bf6f7fbcf
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-03-29 21:03:27 +0900

    rewrite to  random_number_2.py. Additional to write for break, guess <= -1 between 9 to 10, being answer transferred from 5 line to 3 line

--- random_number_2.py
+++ random_number_2.py
@@ -1,11 +1,13 @@
 import random
 
+answer = random.randint(1, 10)
+
 for x in range(3):
-    answer = random.randint(1, 10)
     guess = int(input("好きな数字を入力して下さい: "))
     if answer == guess:
         print("正解!")
-    elif guess >= 11:
+        break
+    elif guess <= -1 or guess >= 11:
         print("数字は1~10までです")
     else:
         print("残念！正解は", answer)
