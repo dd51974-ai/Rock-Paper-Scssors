b211f107ba179809e63b094a203d7bf2f8f0f480
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-03-29 13:09:38 +0900

    rewrite to  random_number_2.py. Additional to write for guess >= 11 to "else;" between 8 to 11

--- random_number_2.py
+++ random_number_2.py
@@ -1,11 +1,11 @@
 import random
 
-answer = random.randint(1,10)
-guess = int(input("好きな数字を入力して下さい: "))
-
-if answer == guess:
-    print("正解!")
-elif guess > answer:
-    print("数字は1~10までです")
-else:
-    print("残念！正解は", answer)
+for x in range(3):
+    answer = random.randint(1, 10)
+    guess = int(input("好きな数字を入力して下さい: "))
+    if answer == guess:
+        print("正解!")
+    elif guess >= 11:
+        print("数字は1~10までです")
+    else:
+        print("残念！正解は", answer)
