803bfd638dd0fca09a0e7acf4526ff4e4a46c68a
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-03-29 11:30:21 +0900

    add to write to elif: sentence

--- random_number.py
+++ random_number.py
@@ -1,10 +1,12 @@
 import random
 
-answer = random.randint(1,10)
+answer = random.randint(1,20)
 
 guess = int(input("1~10の数字を入力してください: "))
 
 if guess == answer:
     print("正解")
+elif guess > answer:
+    print("数字を入力し直してください")
 else:
     print("ハズレ！答えは", answer)
