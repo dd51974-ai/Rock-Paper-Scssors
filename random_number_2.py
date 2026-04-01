9cbc36a3b0c41f6760bddee3309732c183af87ca
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-04-01 14:51:25 +0900

    Add x += 1 on 19 line for while, and write output "Game over" on 20 line of else:

--- random_number_2.py
+++ random_number_2.py
@@ -1,13 +1,21 @@
 import random
+import time
 
 answer = random.randint(1, 10)
-
-for x in range(3):
+count_time = time
+x = 1
+while x <= 3:
     guess = int(input("好きな数字を入力して下さい: "))
+    if guess <= -1 or guess >= 11:
+        print("数字は1~10までです")
     if answer == guess:
         print("正解!")
         break
-    elif guess <= -1 or guess >= 11:
-        print("数字は1~10までです")
+    elif guess > answer:
+        print("惜しい！小さいです")
     else:
-        print("残念！正解は", answer)
+        print("惜しい！大きいです")
+
+    x += 1
+else:
+    print("残念！ゲームオーバーです")
