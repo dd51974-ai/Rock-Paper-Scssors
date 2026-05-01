622c20120b3f47d66c3910c1f1e7bff8c2db00b4
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-05-01 23:23:14 +0900

    add to write between 21 to 23 of random_number_4.py

--- random_number_4.py
+++ random_number_4.py
@@ -7,16 +7,17 @@ game_count = 0 # Counter is 0
 while True:
     games = int(input("数を入力して下さい: "))
     game_count += 1
-    if games > random_number:
-        print("小さい")
-    elif games < random_number:
-        print("大きい")
-    elif game_count == 1 and games == random_number:
+    if game_count == 1 and games == random_number:
         print("素晴らしい！一発で決めましたね！")
         break
-    elif game_count >= 2:
-        print("残念！ゲームオーバーです・・・")
-        break
+    elif games > random_number:
+        print("惜しい！小さいです")
+    elif games < random_number:
+        print("惜しい！大きいです")
     else:
         print("正解！", game_count, "回かかりました")
+        break
+
+    if game_count == 10:
+        print("残念！ゲームオーバーです・・・")
         break
