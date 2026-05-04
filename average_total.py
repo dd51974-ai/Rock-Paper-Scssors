ad14ffdd5cd9e374db282ed65879b91a6c0393f8
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-05-04 17:39:35 +0900

    Success average_total.py

--- average_total.py
+++ average_total.py
@@ -1,19 +1,16 @@
 numbers = [] # Charge number
 while True:
-    input_number = int(input("数を入力して下さい(終了はdone): "))
-    total_amount = sum(numbers.append())
-    average_amount = sum(numbers.append())/len(numbers.append())
-    if input_number < 0:
-        total_amount, average_amount
-        if input_number == "done":
-            total_amount, average_amount
+    input_number = input("数を入力して下さい(終了はdone): ") #文字列で受け取る
+    if input_number == "done":
+        if len(numbers) == 0:
+            print("数が入力されてません")
+        else:# 合計・平均の処理
+            total_amount = sum(numbers)
+            average_amount = sum(numbers) / len(numbers)
             print("合計: ", total_amount)
-            print("平均: ",)
+            print("平均: ", average_amount)
             break
-    elif not input_number:
-        print("まず数字を入力して下さい")
-    elif not input_number and input_number == "done":
-        print("数字が一度も入力されません")
+    elif input_number.isnumeric(): # 数字かどうか判定
+        numbers.append(int(input_number)) #数値に変換
     else:
-        print("数を入力して下さい")
-    break
+        print("数字を入力して下さい")
