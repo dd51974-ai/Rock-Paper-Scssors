3140b2765469c6fa15cd23b3a9cbf5e8c5601876
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-05-08 17:00:13 +0900

    Rewrite to change for elif to if, print to errors.append between 8 to 13 on line, add "if len(errors)" on 14, add errors on 4 line of" password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,15 +1,19 @@
 
 while True:
     pass_word = input("パスワードを入力して下さい: ")
+    errors = []
     if len(pass_word) >= 8 and any(c.isdigit() for c in pass_word) and any(c.isupper() for c in pass_word):
-        print("パスワードが設定されました!")
+        print("⭕️パスワードが設定されました!")
         break
-    elif len(pass_word) < 8:
+    if len(pass_word) < 8:
         print("8文字以上にして下さい")
-    elif not any(c.isdigit() for c in pass_word):
-        print("数字を入れて下さい")
-    elif not any(c.isupper() for c in pass_word):
-        print("英語の大文字を入れて下さい")
+    if not any(c.isdigit() for c in pass_word):
+        errors.append("数字を入れて下さい")
+    if not any(c.isupper() for c in pass_word):
+        errors.append("英語の大文字を入れて下さい")
+    if len(errors) == 0:
+        print("パスワードが設定されました！")
+        break
     else:
-        print("パスワードを入力して下さい")
-
+        for error in errors:
+            print("❌", error)
