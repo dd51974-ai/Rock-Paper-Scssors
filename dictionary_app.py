924313f64b847062078bc89a88f12d16318f3ee2
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-05-09 18:49:48 +0900

    create dictionary_app.py

---
+++ dictionary_app.py
@@ -0,0 +1,21 @@
+dictionary = {}
+while True:
+    print("1: 単語を登録")
+    print("2: 単語を検索")
+    print("3: 終了")
+    choices = input("1~3を選択して下さい: ")
+
+    if choices == "1":
+        choices_word = input("単語を登録して下さい: ")
+        choices_mean = input("意味を入力して下さい: ")
+        dictionary[choices_word] = choices_mean
+        print("登録しました!")
+    elif choices == "2":
+        choices_2 = input("単語を検索して下さい: ")
+        if choices_2 == choices_word:
+            print(dictionary)
+        else:
+            print("!単語が見つかりません!")
+    elif choices == "3":
+        print("終了します")
+        break
