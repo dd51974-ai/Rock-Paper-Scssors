eed0bb89d8d4263577bd744a4cdb2a9eafa76244
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-05-29 23:22:28 +0900

    complete household_account_book.py

--- household_account_book.py
+++ household_account_book.py
@@ -7,7 +7,7 @@ money_list = 0
 
 # Create def save_data():
 def save_data():
-    with open("money_list.json", "w", encodeing="utf-8") as f:
+    with open("money_list.json", "w", encoding="utf-8") as f:
         json.dump({"total_money": total_money, "money_list": money_list}, f, ensure_ascii=False, indent=2)
 
 # save_data()の中に読み込みを入れるのはNG
@@ -38,11 +38,12 @@ while True:
     if choices == "1":
         while True:
             choices_deposit = input("収入金額を記入して下さい: ")
-            if choices_deposit > "0":
-                break
-            elif choices_deposit == "" or not choices_deposit.isdigit():
+            if choices_deposit == "" or not choices_deposit.isdigit(): #数字のみで構成されてるか
                 print("正しい金額を入力して下さい")
                 continue
+            elif int(choices_deposit > "0"):
+                break
+
 
         # 種類の入力を内側のループで繰り返す
         while True:
@@ -62,9 +63,16 @@ while True:
 
         # Add resume
         amount = int(choices_deposit)
-        money_list += amount
+        money_list += amount # Add balance
 
-        total_money = [choices_deposit, choices_type, choices_memo]
+        # Add dictionary methods in resume list 履歴リストに辞書形式で追加＊重要な修正
+        total_money.append({
+            "種別": "収入",
+            "カテゴリ": category_name,
+            "金額": amount,
+            "メモ": choices_memo
+        })
+
         print("{:,}円入金しました！".format(int(choices_deposit)))
         if choices_type == "1":
             print(f"種類: {category_income[choices_type]}", "金額: {:,}円".format(int(choices_deposit)), f"メモ: {choices_memo}")
@@ -75,23 +83,51 @@ while True:
         else:
             print("金額を入力して下さい")
         # Save json 2
-        with open("total_money.json", "w") as f:
-            json.dump(total_money, f)
-
+        save_data()
     # Choice 2 withdrawal
-    if choices == "2":
-        choices_withdrawal = input("支出金額を記入して下さい")
-        # If did not type choices_withdrow
-        withcash = total_money - choices_withdrawal
-        print
+    elif choices == "2":
+        while True:
+            choices_withdrawal = input("支出金額を記入して下さい")
+            if choices_withdrawal == "" or not choices_withdrawal.isdigit():
+                print("金額を入力し直して下さい")
+                continue
+            elif int(choices_withdrawal) > 0:
+                break
+
+        while True:
+            choices_type = input("種類を選んでください 1: 食費, 2: 交通費, 3: その他 ")
+            if choices_type in ("1", "2", "3"):
+                break
+            else:
+                print("指定された数字を選択して下さい")
+                continue
+        category_name = category_expense.get(choices_type, "不明")
+        choices_memo = input("メモを記入して下さい: ")
+        amount = int(choices_withdrawal)
+        money_list -= amount
+        # Add dictionary methods in resume list 履歴リストに辞書形式で追加＊重要な修正
+        total_money.append({
+            "種別": "支出",
+            "カテゴリ": category_name,
+            "金額": amount,
+            "メモ": choices_memo
+        })
+
+        print("{:,}円出金しました!".format(int(choices_withdrawal)))
+        save_data()
+
     # Choice 3 to balance
-    if choices == '3':
-        choices_balance = total_money
-        if choices_balance in money_list:
-            total_money
-            print(total_money, ":", money_list[total_money])
+    elif choices == '3':
+        print("現在の残高{:,}円".format(money_list))
     # Choice 4 resume
+    elif choices == '4':
+        if not total_money:
+            print("履歴がありません")
+        else:
+            for i, record in enumerate(total_money, 1):
+                print(f"{i}. {record['種別']} / {record['カテゴリ']} / {record['金額']:,}円 / {record['メモ']}")
+
     # Choice 5 to end
-    if choices == "5":
+    elif choices == "5":
         print("終了します")
         break
