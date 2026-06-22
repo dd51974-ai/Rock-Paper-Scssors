bf88383f665867c9086af424f60473294d39034a
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-06-22 17:15:36 +0900

    create def save_data():, os as a Roading

--- library.py
+++ library.py
@@ -1,3 +1,19 @@
 import json
 import os
 books = []
+print(len(books))
+
+# Create def save_data():
+def save_data():
+    with open("library_books_list.json", "w", encoding="utf-8") as f:
+        json.dump({"books": books}, f, ensure_ascii=False, indent=1)
+
+# Roading
+if os.path.exists("todo_list.json"):
+    with open("library_books_list.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        tasks = data.get("books", [])
+
+books = {
+
+}
