4202e0d9cfd78a3036bc7c00b8f64b82d3d8d8a1
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-06-25 15:09:28 +0900

    Rewrite choices == "1"

--- library.py
+++ library.py
@@ -17,7 +17,7 @@ if os.path.exists("library_books_list.json"):
 book = {
     "借りる本の番号": books,
     "本の名前": "",
-    "借りる人の名前": "",
+    "借りる人の名前": None,
     "返却": False
 }
 
@@ -27,12 +27,14 @@ while True:
     print("3: 本を返す")
     print("4: 一覧表示")
     print("5: 終了")
-    choices = input("選択して下さい")
+    choices = input("選択して下さい: ")
 
     if choices == "1":
         while True:
-            new_register = input("本を新規登録して下さい")
-            books.append({"借りる本の番号": books, "本の名前": len(""), "借りる人の名前": len(""), "完了": False})
+            print("本の新規登録をお願いします: ")
+            book_title_register = input("本のタイトルを入力して下さい: ")
+
+            books.append({"借りる本の番号": len(books) + 1, "本の名前": book_title_register, "完了": False})
             print(len(books))
             if books == "":
                 print("記入をお願いします")
@@ -44,16 +46,14 @@ while True:
     elif choices == "2":
         # 本を借りる
         while True:
-            check_number = input("本の登録番号を入力して下さい")
+            check_number = input("本の登録番号を入力して下さい: ")
             if books == check_number:
                 print("この番号は存在します")
-                if books == True:
+                if books == False:
                     print("この本は貸し出しできます")
                     # 貸出人の名前
                     borrower = input("お名前を記入して下さい: ")
-                    # 借りる本の名前
-                    rental_book = input("本の名前を記入して下さい")
-                    books.append({"借りる人の名前": borrower, "本の名前": rental_book, "貸出": False})
+                    books.append({"借りる人の名前": borrower, "本の名前": book_title_register, "貸出": True})
                     print(len(books))
                     print("貸出完了")
                     save_data()
@@ -61,3 +61,6 @@ while True:
             else:
                 print("番号が見つかりません")
                 continue
+    elif choices == "5":
+        print("終了します")
+        break
